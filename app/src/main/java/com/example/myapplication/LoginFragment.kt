9c3b8c1a360ff24f42cdb6e10a962046fc2f2f92
Amin <amin.sakha00@gmail.com>
2023-05-20 00:45:19 +0330

    kinda raedy for tomorow

--- app/src/main/java/com/example/myapplication/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/LoginFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.databinding.FragmentLoginBinding
 import com.example.myapplication.databinding.FragmentSplashBinding
 import com.example.myapplication.utils.changeFragment
+import com.example.myapplication.utils.phoneNumber
+import com.example.myapplication.utils.showSnackbar
+import com.example.myapplication.utils.userName
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -29,7 +34,18 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.loginBTN.setOnClickListener {
-            changeFragment(binding.loginBTN, R.id.action_loginFragment_to_bottomNavActivity)
+            userName = binding.userNameTV.editText?.text.toString()
+
+            lifecycleScope.launch {
+                //phoneNumber = read(binding.userNameTV.editText?.text.toString())!!
+                if (read(binding.userNameTV.editText?.text.toString()) != null && binding.pass2TV.editText?.text.toString() == read(
+                        binding.userNameTV.editText?.text.toString()
+                    )
+                ) {
+                    changeFragment(binding.loginBTN, R.id.action_loginFragment_to_bottomNavActivity)
+                } else
+                    showSnackbar(binding.loginBTN, "شما نمیتوانید وارد شوید")
+            }
         }
     }
 
--- app/src/main/java/com/example/myapplication/SignUpFragment.kt
+++ app/src/main/java/com/example/myapplication/SignUpFragment.kt
@@ -6,8 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.databinding.FragmentLoginBinding
 import com.example.myapplication.databinding.FragmentSignUpBinding
+import com.example.myapplication.utils.changeFragment
+import com.example.myapplication.utils.phoneNumber
+import com.example.myapplication.utils.showSnackbar
+import com.example.myapplication.utils.userName
+import kotlinx.coroutines.launch
 
 class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
@@ -25,4 +31,19 @@ class SignUpFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signBTN.setOnClickListener {
+            userName = binding.userNameTV2.editText?.text.toString()
+            phoneNumber = binding.idNumberTvSignUp.editText?.text.toString()
+            lifecycleScope.launch {
+                save(
+                    binding.idNumberTvSignUp.editText?.text.toString(),
+                    binding.passTV.editText?.text.toString()
+                )
+                showSnackbar(binding.signBTN, "شما با موفقیت ثبت نام شدید")
+                changeFragment(binding.signBTN, R.id.action_signUpFragment_to_bottomNavActivity)
+            }
+        }
+    }
 }
